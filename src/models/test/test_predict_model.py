951f547264972096648bb94530a4eea90bbbaea4
vekshan <36571428+vekshan@users.noreply.github.com>
2021-11-21 11:00:14 -0500

    Fix predict_model.py to send data unprocessed

--- src/models/test/test_predict_model.py
+++ src/models/test/test_predict_model.py
@@ -6,17 +6,11 @@ import pandas as pd
 producer = KafkaProducer(bootstrap_servers='localhost:9092' , api_version=(0, 10, 1))
 
 df = pd.read_csv("../../../data/processed/final_df.csv")
-columns = list(df.columns)
-features = columns[4:]
-df[features] = df[features].div(df.total_events, axis = 0)
 
-data = df.iloc[104,4:].values.tobytes()
-producer.send('anomaly', data)
-
-# for _, row in df.iterrows():
-#     data = row.values.tobytes()
-#     future = producer.send('anomaly', data)
-#     result = future.get(timeout=60)
-#     producer.flush()
-# metrics = producer.metrics()
-# print(metrics)
+for _, row in df.iterrows():
+    data = row.values.tobytes()
+    future = producer.send('anomaly', data)
+    result = future.get(timeout=60)
+    producer.flush()
+metrics = producer.metrics()
+print(metrics)
