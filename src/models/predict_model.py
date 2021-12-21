9ba5bdb9736c05d521cad675de6d6ef859b28f81
vekshan <36571428+vekshan@users.noreply.github.com>
2021-12-21 12:20:29 -0500

    Fixed Production Environment (#16)
    * Fix Prod
    
    * Fix indices in predict_model.py
    
    * Add host in anomaly logging
    
    Co-authored-by: Sukhsimranpreet Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,6 +3,7 @@ from pickle import load
 from kafka import KafkaConsumer
 import numpy as np
 from pickle import load
+import json
 
 with open('final_model.sav', 'rb') as f:
     model = load(f)
@@ -11,16 +12,21 @@ with open('final_model.sav', 'rb') as f:
 consumer = KafkaConsumer(
     'anomaly',
     bootstrap_servers='localhost:9092',
-    auto_offset_reset='latest')
+    auto_offset_reset='earliest')
 
 for message in consumer:
-    data = np.frombuffer(message.value, dtype=int)
+    data = message.value.decode('utf-8')
+    # print(data)
+    data = data.split(',')
+    host = data.pop(1)
+    data = [float(i) for i in data]
+    # print(len(data))
+    data = np.array(data)
+    # # data = np.frombuffer(message.value, dtype=int)
     time = data[0]
-    total_events = data[2]
-    features = (data[4:]/total_events).reshape(1,-1)
-    print(features)
-    print (model.predict(X)[0])
+    total_events = data[1]  # removed user, else total_events = data[2]
+    features = (data[3:]/total_events).reshape(1, -1)  # removed user else 4:
+    # print(features)
+    # print(len(features[0]))
     if model.predict(features)[0] == 1:
-        print(f'Anomaly detected at time {str(time)}')
-
-
+        print(f'Anomaly detected at time {str(time)} on host {host}')
--- src/models/test/test_predict_model.py
+++ src/models/test/test_predict_model.py
@@ -1,15 +1,18 @@
 from time import sleep
-from json import dumps
 from kafka import KafkaProducer
 import pandas as pd
+import json
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092' , api_version=(0, 10, 1))
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092', api_version=(0, 10, 1))
 
-df = pd.read_csv("../../../data/processed/final_df.csv")
+df = pd.read_csv("../../../data/processed/final_df.csv", engine='python')
 
 for _, row in df.iterrows():
-    data = row.values.tobytes()
-    future = producer.send('anomaly', data)
+    data = row.values.tolist()
+    data = [str(element) for element in data]
+    data = ",".join(data)
+    future = producer.send('anomaly', data.encode('utf-8'))
     result = future.get(timeout=60)
     producer.flush()
 metrics = producer.metrics()
